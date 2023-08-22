6b1eb6b3cf3c8c4d66aafed86d50f8ed7ba2b5d6
ManuelSchmidt <manne0081@gmail.com>
2023-08-22 17:11:28 +0200

    header-fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PartnerComponent } from './partner/partner.component';
 
 const routes: Routes = [
-    { path: '',   redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: '',   redirectTo: 'home', pathMatch: 'full' },
 
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
