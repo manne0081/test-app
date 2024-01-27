45ab703b8e2b2b8d6d11922b31e8f43c2d8f22db
manne0081@gmail.com <manne0081@gmail.com>
2024-01-27 14:08:18 +0100

    header-fix + icomoon-fonts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CookieService } from 'ngx-cookie-service';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
