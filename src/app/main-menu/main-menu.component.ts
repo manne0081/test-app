aedc6deaed8ec12a7ba7560d7a9088286537b223
manne0081@gmail.com <manne0081@gmail.com>
2024-02-08 23:55:57 +0100

    heaer

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-main-menu',
-  templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.scss']
+    selector: 'app-main-menu',
+    templateUrl: './main-menu.component.html',
+    styleUrls: ['./main-menu.component.scss']
 })
-export class MainMenuComponent {
 
+export class MainMenuComponent implements OnInit {
+    classFavorite: string ="";
+    classDashboard: string ="";
+    classWorkspace: string ="";
+    classContact: string ="";
+
+    constructor (private router: Router) {
+    }
+
+    ngOnInit(): void {
+        this.setItemClass("/dashboard");
+    }
+
+    setItemClass(url: string): void {
+        console.log("url: " + url);
+        if (url == "/dashboard") {
+            this.classFavorite = "pre-active";
+            this.classDashboard = "active";
+            this.classWorkspace = "post-active";
+            this.classContact = "";
+        };
+        if (url == "/workspace") {
+            this.classFavorite = "";
+            this.classDashboard = "pre-active";
+            this.classWorkspace = "active";
+            this.classContact = "post-active";
+        };
+        if (url == "/contact") {
+            this.classFavorite = "";
+            this.classDashboard = "";
+            this.classWorkspace = "pre-active"
+            this.classContact = "active"
+        };
+    }
 }
